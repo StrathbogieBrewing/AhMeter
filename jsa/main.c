37285d78d1536100b45d612928a0ec39cf4d2d35
johny hoops <johnyhoops@gmail.com>
2025-03-29 17:22:05 +1100

    new format jsa working

--- jsa/main.c
+++ jsa/main.c
@@ -21,70 +21,57 @@
 #define MESSAGE_SHUNT_BASE 0x80
 
 #define ADC_SAMPLES 7
-#define BIT_PERIOD_US 88 // 9600 bps with clock at 921.6 kHz (96 less 8 cycles)
-#define BIT_PULSE_US 20  // pulse is approx 3/16 of bit period
+#define BIT_PERIOD_US (96 - 4) // 9600 bps with clock at 921.6 kHz (4 us adjusted for loop delay)
 
 uint8_t getSwitch(void) {
     return (0x0F & (((uint8_t)(~PIND)) >> 3)); // read address from rotary switch
 }
 
-// waits one bit period and returns true if bus is inactive
-static bool bit_sync(void) {
-    bool rx_inactive = true;
-    while ((TIFR & (1 << OCF0A)) == 0) {
-        if (!RX_IS_RELEASED) {
-            rx_inactive = false;
+// returns true if successful
+bool tx_zeros(uint8_t bit_count) {
+    bool rx_inactive = false;
+    if (RX_IS_RELEASED) {
+        TX_ACTIVE;
+        while (bit_count--) {
+            _delay_us(BIT_PERIOD_US);
+            if (!RX_IS_RELEASED) {
+                rx_inactive = true;
+            }
         }
+        TX_RELEASE;
     }
-    TCNT0 = 0;
-    OCR0A = BIT_PERIOD_US;
-    TIFR |= (1 << OCF0A);
     return rx_inactive;
 }
 
 // returns true if successful
-bool tx_zero(void) {
-    if (RX_IS_RELEASED) {
-        TX_ACTIVE;
-        _delay_us(BIT_PULSE_US);
-        TX_RELEASE;
-        _delay_us(BIT_PULSE_US / 2);
-        return bit_sync();
+bool tx_ones(uint8_t bit_count) {
+    bool rx_inactive = true;
+    while (bit_count--) {
+        _delay_us(BIT_PERIOD_US);
+        if (!RX_IS_RELEASED) {
+            rx_inactive = false;
+        }
     }
-    return false;
-}
-
-// returns true if successful
-bool tx_one(void) {
-    return bit_sync();
+    return rx_inactive;
 }
 
 bool send(uint8_t data[], uint8_t bytes_to_send) {
-    for (uint8_t i = 0; i < 20; i++) { // minimum 20 bit periods between bus activity
-        if (!tx_one()) {
-            return false;
-        }
+    if (!tx_ones(20)) { // minimum 20 bit periods between bus activity
+        return false;
     }
     while (bytes_to_send--) {
-        uint8_t mask = 0x80; // send bits in canbus order (msb first)
         uint8_t byte = *data++;
-        if (!tx_zero()) {
-            return false;
-        }
-        while (mask != 0) {
-            if (mask & byte) {
-                if (!tx_one()) {
-                    return false;
-                }
-            } else {
-                if (!tx_zero()) {
-                    return false;
-                }
+        uint8_t nibble = 4;
+        while (nibble) {
+            uint8_t zeros = 4 - (byte >> 6);
+            if (!tx_zeros(zeros)) {
+                return false;
             }
-            mask >>= 1;
-        }
-        if (!tx_one()) {
-            return false;
+            if (!tx_ones(5 - zeros)) {
+                return false;
+            }
+            byte <<= 2;
+            nibble -= 1;
         }
     }
     return true;
@@ -114,8 +101,7 @@ int main(void) {
                 count = 0;
 
                 uint8_t frame[FRAME_SIZE];
-                // set lsb to zero to allow for future 16 bit ids
-                uint8_t id = MESSAGE_SHUNT_BASE | (getSwitch() << 1);
+                uint8_t id = MESSAGE_SHUNT_BASE | getSwitch();
                 frame[0] = id;
                 frame[1] = (uint32_t)current >> 16;
                 frame[2] = (uint32_t)current >> 8;
@@ -136,7 +122,7 @@ int main(void) {
                     tries++;
                     uint8_t i = ((priority & 0x07) << 2) + (TCNT0 & 0x03);
                     while (i--) {
-                        _delay_us(50); // approx 1/2 bit periods
+                        _delay_us(BIT_PERIOD_US / 2); 
                     }
                 }
             }
